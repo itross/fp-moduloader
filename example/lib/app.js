a447400ebf6c3752276c8b890c8dc4ba5c162a10
joshuagame <joshuagame@gmail.com>
2023-06-27 14:16:47 +0200

    moduloader 0.1.0

--- example/lib/app.js
+++ example/lib/app.js
@@ -4,8 +4,7 @@ const moduloader = require('../..')
 
 async function app (fastify, opts) {
   fastify.register(moduloader, {
-    ...opts,
-    dir: '/Users/joshuagame/oss/projects/itross/fp-moduloader/repo/fp-moduloader/example/lib/modules'
+    ...opts
   })
 }
 
